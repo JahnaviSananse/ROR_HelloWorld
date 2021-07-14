62514b75c150a776a343996f1b3b38a9b83d65b6
JahnaviSananse <jahnavisananse@gmail.com>
2021-07-14 13:28:00 +0530

    simple project with hello world message

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,53 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "Hello World !!!!"
+  end
+end
+
+
+# class UsersController < ApplicationController
+#   .
+#   .
+#   .
+#   def index
+#     .
+#     .
+#     .
+#   end
+
+#   def show
+#     .
+#     .
+#     .
+#   end
+
+#   def new
+#     .
+#     .
+#     .
+#   end
+
+#   def edit
+#     .
+#     .
+#     .
+#   end
+
+#   def create
+#     .
+#     .
+#     .
+#   end
+
+#   def update
+#     .
+#     .
+#     .
+#   end
+
+#   def destroy
+#     .
+#     .
+#     .
+#   end
+# end
